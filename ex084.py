a09176f065d8825c21542ac5c08f74210704b1ae
João Victor Oliveira <120576692+Victor-joao1@users.noreply.github.com>
2023-03-14 19:03:34 -0300

    Update ex084.py

---
+++ ex084.py
@@ -0,0 +1,24 @@
+lista = list()
+pessoas = list()
+cont = maisp = menosp = 0
+while True:
+    pessoas.append(str(input('Digite seu nome:')))
+    pessoas.append(float(input('Digite seu peso:')))
+    cont += 1
+    lista.append(pessoas[:])
+    pessoas.clear()
+    continuar = input('Deseja continuar? [S/N]')
+    if continuar in 'Nn':
+        break
+print(lista)
+for p in lista:
+    if p[1] > 75.0:
+        print(f'É {p[0]} uma pessoa mais pesada.')
+        maisp += 1
+    else:
+        print(f'É {p[0]} uma pessoa mais leve.')
+        menosp += 1
+print(f'Temos {maisp} pessoas pesadas e {menosp} pessoas leves.')
+
+
+
