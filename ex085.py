68dc9cce7f2d5e950818781cb7def8b8a9768a44
João Victor Oliveira <120576692+Victor-joao1@users.noreply.github.com>
2023-03-15 11:45:45 -0300

    Create ex085.py

---
+++ ex085.py
@@ -0,0 +1,17 @@
+lista = list()
+valoresi = list()
+valoresp = list()
+for v in range(0, 7):
+    n = int(input('Digite um valor:'))
+    if n % 2 == 0:
+        valoresp.append(n)
+        lista.append(valoresp[:])
+        valoresp.sort()
+        
+    else:
+        valoresi.append(n)
+        lista.append(valoresi[:])
+        valoresi.sort()
+        
+print(f'Os valores pares pares digitados foram {valoresp}')
+print(f'Os valores pares ímpares digitados foram {valoresi}')
